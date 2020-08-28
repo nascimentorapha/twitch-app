1c9fa88bc2815baf05423bc570105e048c17498f
nascimentorapha <nascimento.rapha@hotmail.com>
2020-08-28 16:43:04 -0300

    Coming soon pages finished

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Wrapper, Heading } from './styles';
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Wrapper>
+        <Heading>Coming soon...</Heading>
+      <Text>ComingSoon</Text>
+    </Wrapper>
+  );
+};
+
+export default ComingSoon;

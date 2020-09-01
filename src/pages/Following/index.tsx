2e6316b8716bb850995772e843949cdc9e33b0ea
nascimentorapha <nascimento.rapha@hotmail.com>
2020-09-01 16:51:31 -0300

    created interface for items on page and started FlatList on Main

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,13 +1,83 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, FlatList } from 'react-native';
 
-import { Container } from './styles';
+import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+
+import { Wrapper, Container, Main } from './styles';
+
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
 
 const Following: React.FC = () => {
+  const { data, indices } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <View />,
+      },
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C1', render: () => <View /> },
+      
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C2', render: () => <View /> },
+      
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C3', render: () => <View /> },
+      
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C4', render: () => <View /> },
+    ];
+
+    //Array que contém apenas os indices dos elementos que são títulos
+    const indices: number[] = [];
+
+    items.forEach((item, index) => item.isTitle && indices.push(index));
+
+    return {
+      data: items,
+      indices,
+    }
+  }, [])
+
   return (
-    <Container>
-      <Text>Following</Text>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Header />
+
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({ item }) => item.render()}
+            keyExtractor={item => item.key}
+            stickyHeaderIndices={indices}
+            //Refresh effect
+            onRefresh={() => {}}
+            refreshing={false}
+          />
+          
+        </Main>
+      </Container>
+    </Wrapper>
   );
 };
 

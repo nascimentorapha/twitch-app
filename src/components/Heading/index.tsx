01d7fe8f25f2444ef80937dac5453d0b9138fb29
nascimentorapha <nascimento.rapha@hotmail.com>
2020-09-02 16:44:22 -0300

    configured children parameter and console.log for debug

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { Container, HeadingText } from './styles';
 
-const Heading: React.FC = () => {
+const Heading: React.FC = ({children}) => {
+  console.log("cheguei aqui 3");
   return (
     <Container>
       <HeadingText>{children}</HeadingText>

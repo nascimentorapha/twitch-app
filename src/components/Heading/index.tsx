ec9f4dd93ddb7d09e55fa3728d11dbbfbfec8bed
nascimentorapha <nascimento.rapha@hotmail.com>
2020-09-03 16:51:44 -0300

    removed debug console.logs

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Container, HeadingText } from './styles';
 
 const Heading: React.FC = ({children}) => {
-  console.log("cheguei aqui 3");
   return (
     <Container>
       <HeadingText>{children}</HeadingText>
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -3,10 +3,9 @@ import styled from 'styled-components/native';
 import colors from '../../styles/colors'
 
 export const Container = styled.View`
-    padding-top: 10px 0 20px;
 `;
 
-export const HeadingText = styled.View`
+export const HeadingText = styled.Text`
     color: ${colors.black};
     font-family: roboto_700;
     font-size: 30px;

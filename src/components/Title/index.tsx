552415a8627056bb961a26c629d04fdd2916aad5
nascimentorapha <nascimento.rapha@hotmail.com>
2020-09-03 16:49:08 -0300

    removed debug console.log row

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Container } from './styles';
 
 const Title: React.FC = ({ children }) => {
-  console.log("cheguei aqui 4");
   return (
     <Container>{children}</Container>
   );

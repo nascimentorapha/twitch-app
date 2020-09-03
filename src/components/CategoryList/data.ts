b37f2e7fb73c6c24bf7c0f13d9734217a435fe74
nascimentorapha <nascimento.rapha@hotmail.com>
2020-09-03 16:43:46 -0300

    changed some preview images

--- src/components/CategoryList/data.ts
+++ src/components/CategoryList/data.ts
@@ -1,15 +1,15 @@
 import lol from '../../images/category_lol.jpg';
+import cod from '../../images/category_cod.jpg';
 import valorant from '../../images/category_valorant.jpg';
-import csgo from '../../images/category_csgo.jpg';
-import fortnite from '../../images/category_fortnite.jpg';
+import music from '../../images/category_music.jpg';
 import fallguys from '../../images/category_fallguys.jpg';
 import apex from '../../images/category_apex.jpg';
 
 export default [
   { name: 'League of Legends', source: lol },
   { name: 'VALORANT', source: valorant },
-  { name: 'Counter-Strike: Global Offensive', source: csgo },
-  { name: 'Fortnite', source: fortnite },
+  { name: 'Call of Duty: Modern Warfare', source: cod },
+  { name: 'Music', source: music },
   { name: 'Fall Guys', source: fallguys },
   { name: 'Apex Legends', source: apex },
 ];
